19f64e3696a05cecfdb1bd93480d571090badca7
Gleb4ik2001 <g.kalashnikovvv@gmail.com>
2026-06-14 02:20:43 +0500

    fix: fixed datetime module in sample model & added httpx2 to reqs.txt

--- app/models/sample.py
+++ app/models/sample.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from enum import Enum
 
 from sqlalchemy import DateTime
@@ -45,5 +45,5 @@ class Sample(Base):
 
     created_at: Mapped[datetime] = mapped_column(
         DateTime,
-        default=datetime.utcnow
+        default=lambda: datetime.now(timezone.utc)
     )
